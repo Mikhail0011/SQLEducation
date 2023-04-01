a05d9fe3fd651d788bc6fbe579ff3726129e50f2
Mikhail0011 <misha.karpushin@gmail.com>
2023-04-01 14:22:33 +0300

    Fix the procedure

--- dbo.usp_MakeFamilyPurchase.sql
+++ dbo.usp_MakeFamilyPurchase.sql
@@ -6,17 +6,13 @@ CREATE PROCEDURE dbo.usp_MakeFamilyPurchase
   @FamilySurName varchar(255)
 AS 
 BEGIN
-	DECLARE @valueSum DECIMAL(18,2);
 
 	IF NOT EXISTS(SELECT ID FROM dbo.Family WHERE SurName = @FamilySurName) 
-		PRINT 'No family found with this surname!';	
-	ELSE 	
-		SELECT @valueSum = SUM(bskt."Value")
-		FROM dbo.Basket AS bskt 
-		INNER JOIN dbo.Family AS fml ON fml.ID = bskt.ID_Family
-									AND fml.SurName = @FamilySurName;
-
+		RAISERROR('No family found with this surname!', 10, 1)	
+	ELSE 
 		UPDATE dbo.Family
-		SET BudgetValue = BudgetValue - @valueSum
+		SET BudgetValue = BudgetValue - (SELECT SUM(bskt."Value")
+										 FROM dbo.Basket AS bskt
+										 WHERE bskt.ID_Family = dbo.Family.ID)
 		WHERE SurName = @FamilySurName;
 END;
