4d01879bf21cc3bdc26e0c821378e0ca8a75bb05
Mikhail0011 <misha.karpushin@gmail.com>
2023-04-01 14:16:07 +0300

    Fix the trigger

--- dbo.TR_Basket_insert_update.sql
+++ dbo.TR_Basket_insert_update.sql
@@ -3,10 +3,11 @@ USE localdb;
 GO
 CREATE TRIGGER dbo.TR_Basket_insert_update 
 ON dbo.Basket
-AFTER INSERT AS
+INSTEAD OF INSERT AS
 BEGIN
-	UPDATE dbo.Basket
-	   SET DiscountValue = 
+	INSERT INTO dbo.Basket(ID_SKU, ID_Family, Quantity, "Value", PurchaseDate, DiscountValue)
+	SELECT ID_SKU, ID_Family, Quantity, "Value", PurchaseDate, 
+		   DiscountValue = 
 			CASE WHEN ID_SKU IN (SELECT ID_SKU
 								 FROM inserted
 								 GROUP BY ID_SKU
@@ -14,6 +15,6 @@ BEGIN
 				 THEN Value * 0.05
 			ELSE 0
 			END
-		WHERE ID_SKU IN (SELECT ID_SKU
-						 FROM inserted);
+		FROM inserted;
 END;
+
