e0becb3e634d7e3ad0842fee8d70ce4ca782294d
Mikhail0011 <misha.karpushin@gmail.com>
2023-04-06 00:24:12 +0300

    Update dbo.TR_Basket_insert_update.sql

--- dbo.TR_Basket_insert_update.sql
+++ dbo.TR_Basket_insert_update.sql
@@ -6,18 +6,6 @@ ON dbo.Basket
 INSTEAD OF INSERT AS
 BEGIN
 	INSERT INTO dbo.Basket(ID_SKU, ID_Family, Quantity, "Value", PurchaseDate, DiscountValue)
-<<<<<<< Updated upstream
-	SELECT ID_SKU, ID_Family, Quantity, "Value", PurchaseDate, 
-		   DiscountValue = 
-			CASE WHEN ID_SKU IN (SELECT ID_SKU
-								 FROM inserted
-								 GROUP BY ID_SKU
-							     HAVING COUNT(*) > 1)
-				 THEN Value * 0.05
-			ELSE 0
-			END
-		FROM inserted;
-=======
 	SELECT ins.ID_SKU, ins.ID_Family, ins.Quantity, ins."Value", ins.PurchaseDate, 
 			CASE WHEN ins1.ID_SKU IS NOT NULL
 				 THEN Value * 0.05
@@ -28,6 +16,5 @@ BEGIN
 					 FROM inserted
 					 GROUP BY ID_SKU
 					 HAVING COUNT(*) > 1) AS ins1 ON ins1.ID_SKU = ins.ID_SKU;;
->>>>>>> Stashed changes
 END;
 
